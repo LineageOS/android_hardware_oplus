4aad2002f2a3ebdd416b58244973c3cac41e8eb7
LuK1337 <priv.luk@gmail.com>
2021-03-14 11:29:05 +0100

    livedisplay: Implement IAntiFlicker interface fragment
    Change-Id: I1dbc6d8989a80fe7b06d4356aeb717417997a793

---
+++ hidl/livedisplay/AntiFlicker.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "AntiFlickerService"
+
+#include <livedisplay/oplus/AntiFlicker.h>
+#include <android-base/file.h>
+#include <android-base/strings.h>
+
+namespace {
+
+constexpr const char* kDcDimmingPath = "/sys/kernel/oplus_display/dimlayer_bl_en";
+
+}  // anonymous namespace
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_1 {
+namespace implementation {
+
+Return<bool> AntiFlicker::isEnabled() {
+    std::string tmp;
+    int32_t contents = 0;
+
+    if (ReadFileToString(kDcDimmingPath, &tmp)) {
+        contents = std::stoi(Trim(tmp));
+    }
+
+    return contents > 0;
+}
+
+Return<bool> AntiFlicker::setEnabled(bool enabled) {
+    return WriteStringToFile(std::to_string(enabled), kDcDimmingPath, true);
+}
+
+}  // namespace implementation
+}  // namespace V2_1
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
