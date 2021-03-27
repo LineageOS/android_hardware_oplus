c91471ab081fa913d77faef2c2115859d3767506
LuK1337 <priv.luk@gmail.com>
2021-03-27 17:42:18 +0100

    livedisplay: Uprev to 2.1
    Change-Id: I98b2ea16ed342339ee652d5cb79dcfd22f8c0a0d

--- hidl/livedisplay/service.cpp
+++ hidl/livedisplay/service.cpp
@@ -14,12 +14,13 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.livedisplay@2.0-service-oplus"
+#define LOG_TAG "vendor.lineage.livedisplay@2.1-service-oplus"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
 #include <livedisplay/sdm/PictureAdjustment.h>
+#include <vendor/lineage/livedisplay/2.1/IPictureAdjustment.h>
 
 #include "SunlightEnhancement.h"
 
@@ -31,8 +32,9 @@ using ::android::hardware::joinRpcThreadpool;
 
 using ::vendor::lineage::livedisplay::V2_0::sdm::PictureAdjustment;
 using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
-using ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
-using ::vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_1::IPictureAdjustment;
+using ::vendor::lineage::livedisplay::V2_1::ISunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_1::implementation::SunlightEnhancement;
 
 int main() {
     status_t status = OK;
