555480b55b773a9bbe8088ad290cfc0c0da3df93
LuK1337 <priv.luk@gmail.com>
2021-09-16 18:33:12 +0200

    hidl: Setup touchscreen gestures with Lineage touch HIDL HAL
    This takes advantage of the touchscreen gesture nodes exposed by Oplus
    touchscreen drivers.
    
    Change-Id: I5a43b8f922a8d1c940e367d22d377b7161913d3a

---
+++ hidl/touch/TouchscreenGesture.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.oplus"
+
+#include <touch/oplus/TouchscreenGesture.h>
+#include "touch/oplus/TouchscreenGestureConfig.h"
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+Return<void> TouchscreenGesture::getSupportedGestures(getSupportedGestures_cb resultCb) {
+    std::vector<Gesture> gestures;
+
+    for (const auto &entry : kGestureInfoMap) {
+        gestures.push_back({ entry.first, entry.second.name, entry.second.keycode });
+    }
+    resultCb(gestures);
+
+    return Void();
+}
+
+Return<bool> TouchscreenGesture::setGestureEnabled(
+    const ::vendor::lineage::touch::V1_0::Gesture &, bool) {
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
