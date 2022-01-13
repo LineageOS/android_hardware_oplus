a1dd680ee893fa81d78b951eb8742920c3136bd8
Bruno Martins <bgcngm@gmail.com>
2022-01-13 20:44:10 +0000

    powershare: Use pragma once
    Change-Id: I9cfeed5e79c35139f7532c522dad0ca25ed35fe1

--- hidl/powershare/PowerShare.h
+++ hidl/powershare/PowerShare.h
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef VENDOR_LINEAGE_POWERSHARE_V1_0_POWERSHARE_H
-#define VENDOR_LINEAGE_POWERSHARE_V1_0_POWERSHARE_H
+
+#pragma once
 
 #include <vendor/lineage/powershare/1.0/IPowerShare.h>
 
@@ -41,5 +41,3 @@ class PowerShare : public IPowerShare {
 }  // namespace powershare
 }  // namespace lineage
 }  // namespace vendor
-
-#endif  // VENDOR_LINEAGE_POWERSHARE_V1_0_POWERSHARE_H
