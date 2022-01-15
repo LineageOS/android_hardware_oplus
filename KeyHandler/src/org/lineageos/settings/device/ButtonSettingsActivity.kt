a446bb68c6a75448129b9fefa5e95dffcaad036e
Bruno Martins <bgcngm@gmail.com>
2022-01-15 14:40:54 +0000

    KeyHandler: Update to S
    Change-Id: I68c6c0bc4d02f515a75293150e45b5bb083b0b26

--- KeyHandler/src/org/lineageos/settings/device/ButtonSettingsActivity.kt
+++ KeyHandler/src/org/lineageos/settings/device/ButtonSettingsActivity.kt
@@ -1,19 +1,20 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021-2022 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.lineageos.settings.device
 
-import android.R
-import android.preference.PreferenceActivity
 import android.os.Bundle
 
-class ButtonSettingsActivity : PreferenceActivity() {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
+import com.android.settingslib.collapsingtoolbar.R
+
+class ButtonSettingsActivity : CollapsingToolbarBaseActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fragmentManager.beginTransaction().replace(
-            R.id.content,
+            R.id.content_frame,
             ButtonSettingsFragment()
         ).commit()
     }
