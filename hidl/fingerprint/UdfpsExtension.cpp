3dacefed22089b909d5b5880360962347dc8c67c
LuK1337 <priv.luk@gmail.com>
2022-02-13 20:09:15 +0100

    fingerprint: Add support for TARGET_USES_FOD_ZPOS
    Change-Id: Iaf79d5393169b986197be058ac2166d2e448dfa0

--- hidl/fingerprint/UdfpsExtension.cpp
+++ hidl/fingerprint/UdfpsExtension.cpp
@@ -15,9 +15,14 @@
  */
 
 #include <compositionengine/UdfpsExtension.h>
+#include <drm/sde_drm.h>
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
+#ifdef FOD_PRESSED_LAYER_ZORDER
+    return touched ? z | FOD_PRESSED_LAYER_ZORDER : z;
+#else
     return touched ? 0x41000033 : z;
+#endif
 }
 
 uint64_t getUdfpsUsageBits(uint64_t usageBits, bool /*touched*/) {
