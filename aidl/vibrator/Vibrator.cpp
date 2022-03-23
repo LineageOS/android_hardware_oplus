5af4a415d8a6ceb4e01e7b26364df503cb007cc9
Subbaraman Narayanamurthy <quic_subbaram@quicinc.com>
2022-03-23 11:05:31 -0700

    Vibrator: add support for kalama
    Kalama SOC uses PMIC which has haptics module that can support
    external control. Add the entry for it.
    
    Change-Id: I49e714837062840f180a4b9e0ed89125cb71c781

--- aidl/vibrator/Vibrator.cpp
+++ aidl/vibrator/Vibrator.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -152,6 +153,7 @@ InputFFDevice::InputFFDevice()
             case APQ_CPU_SM8325P:
             case MSM_CPU_TARO:
             case MSM_CPU_YUPIK:
+            case MSM_CPU_KALAMA:
                 mSupportExternalControl = true;
                 break;
             default:
