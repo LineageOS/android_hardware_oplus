99f10d3d04f32a3b2e15181b6c359c7a2bafe1c2
LuK1337 <priv.luk@gmail.com>
2022-04-10 21:04:40 +0200

    KeyHandler: Adjust for Oplus
    Change-Id: Icc5e6583f48756fc765970c9e7a0f27425f37105

--- KeyHandler/tri-state-key-calibrate.sh
+++ KeyHandler/tri-state-key-calibrate.sh
@@ -2,5 +2,5 @@
 if [[ -f /mnt/vendor/persist/engineermode/tri_state_hall_data ]]; then
     CALIBRATION_DATA="$(cat /mnt/vendor/persist/engineermode/tri_state_hall_data)"
     CALIBRATION_DATA="${CALIBRATION_DATA//;/,}"
-    echo -n $CALIBRATION_DATA > /sys/devices/platform/soc/soc:tri_state_key/hall_data_calib
+    echo -n $CALIBRATION_DATA > /proc/tristatekey/hall_data_calib
 fi
