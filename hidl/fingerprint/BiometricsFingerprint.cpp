7f4f347c2c926bfb52e166a2b4cd50c7c7f44ea4
LuK1337 <priv.luk@gmail.com>
2022-04-11 16:38:46 +0200

    hidl: fingerprint: Initial AOSP -> Oplus wrapper
    Change-Id: I8729b3920dd52c18416179d4118a4d012fb5b1d3

---
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.oplus"
+
+#include "BiometricsFingerprint.h"
+
+namespace android {
+namespace hardware {
+namespace biometrics {
+namespace fingerprint {
+namespace V2_3 {
+namespace implementation {
+
+BiometricsFingerprint::BiometricsFingerprint() {
+    mOplusBiometricsFingerprint = IOplusBiometricsFingerprint::getService();
+    mOplusBiometricsFingerprint->setHalCallback(this);
+}
+
+Return<uint64_t> BiometricsFingerprint::setNotify(
+        const sp<V2_1::IBiometricsFingerprintClientCallback>& clientCallback) {
+    mClientCallback = std::move(clientCallback);
+    return mOplusBiometricsFingerprint->setNotify(this);
+}
+
+Return<uint64_t> BiometricsFingerprint::preEnroll() {
+    return mOplusBiometricsFingerprint->preEnroll();
+}
+
+Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69>& hat,
+                                                    uint32_t gid, uint32_t timeoutSec) {
+    return mOplusBiometricsFingerprint->enroll(hat, gid, timeoutSec);
+}
+
+Return<RequestStatus> BiometricsFingerprint::postEnroll() {
+    return mOplusBiometricsFingerprint->postEnroll();
+}
+
+Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
+    return mOplusBiometricsFingerprint->getAuthenticatorId();
+}
+
+Return<RequestStatus> BiometricsFingerprint::cancel() {
+    return mOplusBiometricsFingerprint->cancel();
+}
+
+Return<RequestStatus> BiometricsFingerprint::enumerate() {
+    return mOplusBiometricsFingerprint->enumerate();
+}
+
+Return<RequestStatus> BiometricsFingerprint::remove(uint32_t gid, uint32_t fid) {
+    return mOplusBiometricsFingerprint->remove(gid, fid);
+}
+
+Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid,
+                                                            const hidl_string& storePath) {
+    return mOplusBiometricsFingerprint->setActiveGroup(gid, storePath);
+}
+
+Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId, uint32_t gid) {
+    return mOplusBiometricsFingerprint->authenticate(operationId, gid);
+}
+
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t sensorID) {
+    return mOplusBiometricsFingerprint->isUdfps(sensorID);
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t x, uint32_t y, float minor, float major) {
+    return mOplusBiometricsFingerprint->onFingerDown(x, y, minor, major);
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    return mOplusBiometricsFingerprint->onFingerUp();
+}
+
+Return<void> BiometricsFingerprint::onEnrollResult(uint64_t deviceId, uint32_t fingerId,
+                                                   uint32_t groupId, uint32_t remaining) {
+    return mClientCallback->onEnrollResult(deviceId, fingerId, groupId, remaining);
+}
+
+Return<void> BiometricsFingerprint::onAcquired(uint64_t deviceId,
+                                               V2_1::FingerprintAcquiredInfo acquiredInfo,
+                                               int32_t vendorCode) {
+    return mClientCallback->onAcquired(deviceId, acquiredInfo, vendorCode);
+}
+
+Return<void> BiometricsFingerprint::onAuthenticated(uint64_t deviceId, uint32_t fingerId,
+                                                    uint32_t groupId,
+                                                    const hidl_vec<uint8_t>& token) {
+    return mClientCallback->onAuthenticated(deviceId, fingerId, groupId, token);
+}
+
+Return<void> BiometricsFingerprint::onError(uint64_t deviceId, FingerprintError error,
+                                            int32_t vendorCode) {
+    return mClientCallback->onError(deviceId, error, vendorCode);
+}
+
+Return<void> BiometricsFingerprint::onRemoved(uint64_t deviceId, uint32_t fingerId,
+                                              uint32_t groupId, uint32_t remaining) {
+    return mClientCallback->onRemoved(deviceId, fingerId, groupId, remaining);
+}
+
+Return<void> BiometricsFingerprint::onEnumerate(uint64_t deviceId, uint32_t fingerId,
+                                                uint32_t groupId, uint32_t remaining) {
+    return mClientCallback->onEnumerate(deviceId, fingerId, groupId, remaining);
+}
+
+Return<void> BiometricsFingerprint::onAcquired_2_2(uint64_t deviceId,
+                                                   FingerprintAcquiredInfo acquiredInfo,
+                                                   int32_t vendorCode) {
+    return reinterpret_cast<V2_2::IBiometricsFingerprintClientCallback*>(mClientCallback.get())
+            ->onAcquired_2_2(deviceId, acquiredInfo, vendorCode);
+}
+
+Return<void> BiometricsFingerprint::onEngineeringInfoUpdated(
+        uint32_t /*lenth*/, const hidl_vec<uint32_t>& /*keys*/,
+        const hidl_vec<hidl_string>& /*values*/) {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerprintCmd(int32_t /*cmdId*/,
+                                                     const hidl_vec<uint32_t>& /*result*/,
+                                                     uint32_t /*resultLen*/) {
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V2_3
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace hardware
+}  // namespace android
---
+++ hidl/fingerprint/BiometricsFingerprint.h
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <android/hardware/biometrics/fingerprint/2.1/types.h>
+#include <android/hardware/biometrics/fingerprint/2.2/IBiometricsFingerprintClientCallback.h>
+#include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
+#include <android/log.h>
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <log/log.h>
+
+#include <vendor/oplus/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
+
+namespace android {
+namespace hardware {
+namespace biometrics {
+namespace fingerprint {
+namespace V2_3 {
+namespace implementation {
+
+using ::android::sp;
+using ::android::hardware::hidl_string;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::biometrics::fingerprint::V2_1::FingerprintError;
+using ::android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
+using ::android::hardware::biometrics::fingerprint::V2_2::FingerprintAcquiredInfo;
+using ::android::hardware::biometrics::fingerprint::V2_2::IBiometricsFingerprintClientCallback;
+using ::android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
+
+using IOplusBiometricsFingerprint =
+        vendor::oplus::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
+using ::vendor::oplus::hardware::biometrics::fingerprint::V2_1::
+        IBiometricsFingerprintClientCallbackEx;
+
+class BiometricsFingerprint : public IBiometricsFingerprint,
+                              public IBiometricsFingerprintClientCallback,
+                              public IBiometricsFingerprintClientCallbackEx {
+  public:
+    BiometricsFingerprint();
+
+    // Methods from ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint
+    // follow.
+    Return<uint64_t> setNotify(
+            const sp<V2_1::IBiometricsFingerprintClientCallback>& clientCallback) override;
+    Return<uint64_t> preEnroll() override;
+    Return<RequestStatus> enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid,
+                                 uint32_t timeoutSec) override;
+    Return<RequestStatus> postEnroll() override;
+    Return<uint64_t> getAuthenticatorId() override;
+    Return<RequestStatus> cancel() override;
+    Return<RequestStatus> enumerate() override;
+    Return<RequestStatus> remove(uint32_t gid, uint32_t fid) override;
+    Return<RequestStatus> setActiveGroup(uint32_t gid, const hidl_string& storePath) override;
+    Return<RequestStatus> authenticate(uint64_t operationId, uint32_t gid) override;
+
+    // Methods from ::android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint
+    // follow.
+    Return<bool> isUdfps(uint32_t sensorID) override;
+    Return<void> onFingerDown(uint32_t x, uint32_t y, float minor, float major) override;
+    Return<void> onFingerUp() override;
+
+    // Methods from
+    // ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprintClientCallback
+    // follow.
+    Return<void> onEnrollResult(uint64_t deviceId, uint32_t fingerId, uint32_t groupId,
+                                uint32_t remaining) override;
+    Return<void> onAcquired(uint64_t deviceId, V2_1::FingerprintAcquiredInfo acquiredInfo,
+                            int32_t vendorCode) override;
+    Return<void> onAuthenticated(uint64_t deviceId, uint32_t fingerId, uint32_t groupId,
+                                 const hidl_vec<uint8_t>& token) override;
+    Return<void> onError(uint64_t deviceId, FingerprintError error, int32_t vendorCode) override;
+    Return<void> onRemoved(uint64_t deviceId, uint32_t fingerId, uint32_t groupId,
+                           uint32_t remaining) override;
+    Return<void> onEnumerate(uint64_t deviceId, uint32_t fingerId, uint32_t groupId,
+                             uint32_t remaining) override;
+
+    // Methods from
+    // ::android::hardware::biometrics::fingerprint::V2_2::IBiometricsFingerprintClientCallback
+    // follow.
+    Return<void> onAcquired_2_2(uint64_t deviceId, FingerprintAcquiredInfo acquiredInfo,
+                                int32_t vendorCode) override;
+
+    // Methods from
+    // ::vendor::oplus::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprintClientCallbackEx
+    // follow.
+    Return<void> onEngineeringInfoUpdated(uint32_t lenth, const hidl_vec<uint32_t>& keys,
+                                          const hidl_vec<hidl_string>& values) override;
+    Return<void> onFingerprintCmd(int32_t cmdId, const hidl_vec<uint32_t>& result,
+                                  uint32_t resultLen) override;
+
+  private:
+    sp<IOplusBiometricsFingerprint> mOplusBiometricsFingerprint;
+    sp<V2_1::IBiometricsFingerprintClientCallback> mClientCallback;
+};
+
+}  // namespace implementation
+}  // namespace V2_3
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace hardware
+}  // namespace android
