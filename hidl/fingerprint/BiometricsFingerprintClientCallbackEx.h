86e714bc4982057e1ab2919d57d6aecd1454bfac
LuK1337 <priv.luk@gmail.com>
2022-04-11 16:38:46 +0200

    hidl: fingerprint: Initial AOSP -> Oplus wrapper
    Change-Id: I8729b3920dd52c18416179d4118a4d012fb5b1d3

---
+++ hidl/fingerprint/BiometricsFingerprintClientCallbackEx.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <vendor/oplus/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprintClientCallbackEx.h>
+
+namespace android {
+namespace hardware {
+namespace biometrics {
+namespace fingerprint {
+namespace V2_3 {
+namespace implementation {
+
+using ::android::hardware::hidl_vec;
+using ::android::hardware::hidl_string;
+using ::android::hardware::Return;
+using ::vendor::oplus::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprintClientCallbackEx;
+
+class BiometricsFingerprintClientCallbackEx : public IBiometricsFingerprintClientCallbackEx {
+public:
+    Return<void> onEngineeringInfoUpdated(uint32_t lenth, const hidl_vec<uint32_t> &keys, const hidl_vec<hidl_string> &values) override;
+    Return<void> onFingerprintCmd(int32_t cmdId, const hidl_vec<uint32_t> &result, uint32_t resultLen) override;
+};
+
+}  // namespace implementation
+}  // namespace V2_3
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace hardware
+}  // namespace android
