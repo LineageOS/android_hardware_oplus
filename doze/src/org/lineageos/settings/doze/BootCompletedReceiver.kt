LineageOS/android_hardware_oneplus

doze: Java -> Kotlin + bp -> mk
Change-Id: I377658589af22c58ddbf1768f4c89d06594d2762

doze/src/org/lineageos/settings/doze/BootCompletedReceiver.kt
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.doze
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Starting")
+        Utils.checkDozeService(context)
+    }
+
+    companion object {
+        private const val TAG = "OnePlusDoze"
+    }
+}

LineageOS/android_hardware_oplus

doze/src/org/lineageos/settings/doze/BootCompletedReceiver.kt
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.doze
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Starting")
+        Utils.checkDozeService(context)
+    }
+
+    companion object {
+        private const val TAG = "OplusDoze"
+    }
+}