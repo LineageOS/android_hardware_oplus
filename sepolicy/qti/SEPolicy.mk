eb50d7c01c8270c7df534828c2379ca720a0fbdf
Alexander Koskovich <akoskovich@pm.me>
2022-04-11 23:12:38 -0700

    Add initial QTI SEPolicy for Oplus
    Change-Id: Ib06cada5e7031d1f95976bf48a6a8475a835c9a4

---
+++ sepolicy/qti/SEPolicy.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/public
