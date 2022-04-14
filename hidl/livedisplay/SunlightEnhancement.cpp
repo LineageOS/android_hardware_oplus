LineageOS/android_hardware_oneplus

livedisplay: Expose headers and implementations as Soong modules
Change-Id: I335cb98b544fceed1e0d11f0a7dd9cc38860b5aa

hidl/livedisplay/SunlightEnhancement.cpp
@@ -16,7 +16,7 @@
 
 #define LOG_TAG "SunlightEnhancementService"
 
-#include "SunlightEnhancement.h"
+#include <livedisplay/oneplus/SunlightEnhancement.h>
 #include <android-base/logging.h>
 #include <fstream>
 

hidl/livedisplay/service.cpp
@@ -19,11 +19,10 @@
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
+#include <livedisplay/oneplus/AntiFlicker.h>
+#include <livedisplay/oneplus/SunlightEnhancement.h>
 #include <livedisplay/sdm/PictureAdjustment.h>
 #include <vendor/lineage/livedisplay/2.1/IPictureAdjustment.h>
-
-#include "AntiFlicker.h"
-#include "SunlightEnhancement.h"
 
 using ::android::OK;
 using ::android::sp;

LineageOS/android_hardware_oplus

hidl/livedisplay/SunlightEnhancement.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#include "SunlightEnhancement.h"
+#include <livedisplay/oplus/SunlightEnhancement.h>
 #include <android-base/file.h>
 #include <android-base/strings.h>
 

hidl/livedisplay/service.cpp
@@ -19,10 +19,9 @@
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
+#include <livedisplay/oplus/SunlightEnhancement.h>
 #include <livedisplay/sdm/PictureAdjustment.h>
 #include <vendor/lineage/livedisplay/2.1/IPictureAdjustment.h>
-
-#include "SunlightEnhancement.h"
 
 using ::android::OK;
 using ::android::sp;