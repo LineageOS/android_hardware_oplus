f036dede0801d892f454881121716624e95d33da
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 20:41:13 -0700

    oplus: Import cleaned QSSI RROs from LE2115.11.C.48
     * Remove anything that the value is already the default,
       or deprecated.
    
    Change-Id: I597a93b580754a7fde51d051accfffca54c22482

---
+++ overlay/qssi/qssi.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    FrameworksResCommon \
+    SystemUIResCommon \
+    TelephonyResCommon \
+    WifiResCommon
