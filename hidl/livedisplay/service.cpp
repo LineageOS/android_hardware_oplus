804843613250d74616bcece3246d1cc34baa2132
pjgowtham <pjgowtham@gmail.com>
2022-08-23 12:35:05 +0530

    livedisplay: Fix AntiFlicker
    Change-Id: Ibf382faaa880f24028243b5088d127e277c15cae

--- hidl/livedisplay/service.cpp
+++ hidl/livedisplay/service.cpp
@@ -19,6 +19,7 @@
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
+#include <livedisplay/oplus/AntiFlicker.h>
 #include <livedisplay/oplus/SunlightEnhancement.h>
 #include <livedisplay/sdm/PictureAdjustment.h>
 #include <vendor/lineage/livedisplay/2.1/IPictureAdjustment.h>
@@ -31,9 +32,11 @@ using ::android::hardware::joinRpcThreadpool;
 
 using ::vendor::lineage::livedisplay::V2_0::sdm::PictureAdjustment;
 using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
+using ::vendor::lineage::livedisplay::V2_1::IAntiFlicker;
 using ::vendor::lineage::livedisplay::V2_1::IPictureAdjustment;
 using ::vendor::lineage::livedisplay::V2_1::ISunlightEnhancement;
 using ::vendor::lineage::livedisplay::V2_1::implementation::SunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_1::implementation::AntiFlicker;
 
 int main() {
     status_t status = OK;
@@ -44,10 +47,18 @@ int main() {
 
     std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
 
+    sp<AntiFlicker> af = new AntiFlicker();
     sp<PictureAdjustment> pa = new PictureAdjustment(controller);
     sp<SunlightEnhancement> se = new SunlightEnhancement();
 
     configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    status = af->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Could not register service for LiveDisplay HAL AntiFlicker Iface ("
+                   << status << ")";
+        goto shutdown;
+    }
 
     status = pa->registerAsService();
     if (status != OK) {
