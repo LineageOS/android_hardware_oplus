5b288783bca48486ff30b32f7224817c46a82fdc
LuK1337 <priv.luk@gmail.com>
2023-07-15 16:07:23 +0200

    sepolicy: qti: Add platform specific dirs
    This will let us put um or legacy-um rules in its own dir.
    
    Change-Id: I9e48638c49f0e1727927e4077c56402ad4dee33f

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -1,8 +1,22 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-BOARD_VENDOR_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/public
+ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr-legacy-um)
+    SEPOLICY_PLATFORM := legacy-um
+else
+    SEPOLICY_PLATFORM := um
+endif
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/vendor \
+    hardware/oplus/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/private \
+    hardware/oplus/sepolicy/qti/private/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/public \
+    hardware/oplus/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
