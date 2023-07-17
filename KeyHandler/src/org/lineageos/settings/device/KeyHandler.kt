5179e0d154327c3491ceaf7e4b6569cc5f63eae2
Nick Reuter <nreuter85@gmail.com>
2023-07-16 21:31:56 -0500

    KeyHandler: Initial support for UI notifications
    This is a port of https://gerrit.aospa.co/25761, reworked into an
    OverlayPlugin from SystemUI. Updates are broadcasted to ensure correct
    position and information.
    Available states: Off, Show (Left), Show (Right).
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: Ibeb50e7e24c3d6130170febaf3e4a5233a829e52

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -22,7 +22,7 @@ import com.android.internal.os.DeviceKeyHandler
 import java.io.File
 import java.util.concurrent.Executors
 
-class KeyHandler(context: Context) : DeviceKeyHandler {
+class KeyHandler(private val context: Context) : DeviceKeyHandler {
     private val audioManager = context.getSystemService(AudioManager::class.java)!!
     private val notificationManager = context.getSystemService(NotificationManager::class.java)!!
     private val vibrator = context.getSystemService(Vibrator::class.java)!!
@@ -52,7 +52,7 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
                     }
                 }
 
-                Intent.ACTION_BOOT_COMPLETED -> populateKeyState(false)
+                Intent.ACTION_BOOT_COMPLETED -> populateKeyState(true)
             }
         }
     }
@@ -78,16 +78,16 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
             return event
         }
 
-        populateKeyState(true)
+        populateKeyState(false)
 
         return null
     }
 
-    private fun populateKeyState(vibrate: Boolean) {
+    private fun populateKeyState(firstRun: Boolean) {
         when (File("/proc/tristatekey/tri_state").readText().trim()) {
-            "1" -> handleMode(POSITION_TOP, vibrate)
-            "2" -> handleMode(POSITION_MIDDLE, vibrate)
-            "3" -> handleMode(POSITION_BOTTOM, vibrate)
+            "1" -> handleMode(POSITION_TOP, firstRun)
+            "2" -> handleMode(POSITION_MIDDLE, firstRun)
+            "3" -> handleMode(POSITION_BOTTOM, firstRun)
         }
     }
 
@@ -104,7 +104,9 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         }
     }
 
-    private fun handleMode(position: Int, vibrate: Boolean) {
+    private fun handleMode(position: Int, firstRun: Boolean) {
+        val display = sharedPreferences.getString(ALERT_SLIDER_NOTIFICATION, "2")!!.toInt()
+
         val muteMedia = sharedPreferences.getBoolean(MUTE_MEDIA_WITH_SILENT, false)
 
         val mode = when (position) {
@@ -140,7 +142,8 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
                 }
             }
 
-            if (vibrate) {
+            if (!firstRun) {
+                sendNotification(position, mode, display)
                 vibrateIfNeeded(mode)
             }
         }
@@ -156,15 +159,24 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         }
     }
 
+    private fun sendNotification(position: Int, mode: Int, display: Int) {
+        context.sendBroadcast(Intent(CHANGED_ACTION).apply {
+            putExtra("position", position)
+            putExtra("mode", mode)
+            putExtra("display", display)
+        })
+    }
+
     companion object {
         private const val TAG = "KeyHandler"
 
         // Slider key positions
-        private const val POSITION_TOP = 1
-        private const val POSITION_MIDDLE = 2
-        private const val POSITION_BOTTOM = 3
+        const val POSITION_TOP = 1
+        const val POSITION_MIDDLE = 2
+        const val POSITION_BOTTOM = 3
 
         // Preference keys
+        private const val ALERT_SLIDER_NOTIFICATION = "config_notifications"
         private const val ALERT_SLIDER_TOP_KEY = "config_top_position"
         private const val ALERT_SLIDER_MIDDLE_KEY = "config_middle_position"
         private const val ALERT_SLIDER_BOTTOM_KEY = "config_bottom_position"
@@ -172,9 +184,9 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
 
         // ZEN constants
         private const val ZEN_OFFSET = 2
-        private const val ZEN_PRIORITY_ONLY = 3
-        private const val ZEN_TOTAL_SILENCE = 4
-        private const val ZEN_ALARMS_ONLY = 5
+        const val ZEN_PRIORITY_ONLY = 3
+        const val ZEN_TOTAL_SILENCE = 4
+        const val ZEN_ALARMS_ONLY = 5
 
         // Vibration attributes
         private val HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES =
@@ -183,5 +195,8 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         // Vibration effects
         private val MODE_NORMAL_EFFECT = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK)
         private val MODE_VIBRATION_EFFECT = VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK)
+
+        // Intent actions
+        const val CHANGED_ACTION = "org.lineageos.settings.UPDATE_SETTINGS"
     }
 }
