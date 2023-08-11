b384f35a4d019e3b95deef5e824efdc15062ec30
Nick Reuter <nreuter85@gmail.com>
2023-08-10 21:22:00 -0500

    KeyHandler: make dialog placement configurable
    Change-Id: Ifec64ab07cc7698d6a399eb34bf742fff825dfcf

--- KeyHandler/src/org/lineageos/settings/device/AlertSliderDialog.kt
+++ KeyHandler/src/org/lineageos/settings/device/AlertSliderDialog.kt
@@ -60,13 +60,21 @@ class AlertSliderDialog(context: Context) : Dialog(context, R.style.alert_slider
                 Gravity.TOP or Gravity.RIGHT
             }
 
-            x = context.resources.displayMetrics.widthPixels / 100
+            x = context.resources.getDimension(
+                R.dimen.alert_slider_window_position_x
+            ).toInt()
             y = context.resources.getDimension(
-                com.android.internal.R.dimen.status_bar_height
+                R.dimen.alert_slider_window_position_y
             ).toInt()
 
-            val step = context.resources.displayMetrics.heightPixels / 10
-            y += step * position
+            val shift = context.resources.getDimension(
+                R.dimen.alert_slider_dialog_height
+            ).toInt()
+            when (position) {
+                KeyHandler.POSITION_TOP -> y -= shift
+                KeyHandler.POSITION_BOTTOM -> y += shift
+                else -> {}
+            }
         }
 
         frameView.setBackgroundResource(when (position) {
