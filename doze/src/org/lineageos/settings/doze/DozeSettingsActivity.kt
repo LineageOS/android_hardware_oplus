6e2909a1f9e0d820edc2832ea5bec81a48caa2e1
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    doze/KeyHandler: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- doze/src/org/lineageos/settings/doze/DozeSettingsActivity.kt
+++ doze/src/org/lineageos/settings/doze/DozeSettingsActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 The LineageOS Project
+ * Copyright (C) 2021-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -7,15 +7,17 @@ package org.lineageos.settings.doze
 
 import android.os.Bundle
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
-import com.android.settingslib.widget.R
 
 class DozeSettingsActivity : CollapsingToolbarBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fragmentManager
             .beginTransaction()
-            .replace(R.id.content_frame, DozeSettingsFragment(), TAG)
-            .commit()
+            .replace(
+                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                DozeSettingsFragment(),
+                TAG
+            ).commit()
     }
 
     companion object {
