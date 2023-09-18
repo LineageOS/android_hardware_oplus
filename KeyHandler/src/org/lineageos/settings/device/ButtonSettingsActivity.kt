6526bfaa6a06d2be47a16dcbdfa20f6cbbc36ca8
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    oplus: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- KeyHandler/src/org/lineageos/settings/device/ButtonSettingsActivity.kt
+++ KeyHandler/src/org/lineageos/settings/device/ButtonSettingsActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 The LineageOS Project
+ * Copyright (C) 2021-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -8,13 +8,12 @@ package org.lineageos.settings.device
 import android.os.Bundle
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
-import com.android.settingslib.widget.R
 
 class ButtonSettingsActivity : CollapsingToolbarBaseActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fragmentManager.beginTransaction().replace(
-            R.id.content_frame,
+            com.android.settingslib.collapsingtoolbar.R.id.content_frame,
             ButtonSettingsFragment()
         ).commit()
     }
