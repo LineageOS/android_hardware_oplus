56fff6dd51cace586af7cdb09891e3c98d6a46a6
Georg Veichtlbauer <georg@vware.at>
2023-12-03 12:27:38 +0100

    overlay: qssi: Enable automatic brightness by default
    Change-Id: Iccea0b55a09ce1e49111f9dec8b4ece4e1edfc02

--- overlay/qssi/qssi.mk
+++ overlay/qssi/qssi.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,6 +7,7 @@
 PRODUCT_PACKAGES += \
     FrameworksResCommon \
     OPlusWifiResCommon \
+    SettingsProviderResCommon \
     SystemUIResCommon \
     TelephonyResCommon \
     WifiResCommon
