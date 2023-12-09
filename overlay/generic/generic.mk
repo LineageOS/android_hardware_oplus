1e2a76c601c05049377ea018b30610d3662dd797
Georg Veichtlbauer <georg@vware.at>
2023-12-09 16:21:25 +0100

    overlay: Move EUICC overlay to generic
    Change-Id: I8ed2013aebcb102d75ff7301b43896c99e195e9a

---
+++ overlay/generic/generic.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    FrameworksResGeneric
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
