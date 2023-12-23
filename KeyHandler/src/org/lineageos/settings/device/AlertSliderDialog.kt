ee2539616897bbe906d8329877eefe9defa13553
Ido Ben-Hur <idoybh2@gmail.com>
2023-12-23 16:10:00 +0200

    KeyHandler: Make AlertSliderDialog respect screen rotation
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: Icdc8bcc48512bb2edbbbc1757c969cf7082ab445

--- KeyHandler/src/org/lineageos/settings/device/AlertSliderDialog.kt
+++ KeyHandler/src/org/lineageos/settings/device/AlertSliderDialog.kt
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: 2019 CypherOS
  * SPDX-FileCopyrightText: 2014-2020 Paranoid Android
  * SPDX-FileCopyrightText: 2023-2024 The LineageOS Project
+ * SPDX-FileCopyrightText: 2023 Yet Another AOSP Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -14,6 +15,7 @@ import android.graphics.PixelFormat
 import android.graphics.drawable.ColorDrawable
 import android.media.AudioManager
 import android.view.Gravity
+import android.view.Surface
 import android.view.ViewGroup
 import android.view.Window
 import android.view.WindowManager
@@ -30,7 +32,14 @@ class AlertSliderDialog(private val context: Context)
     private val iconView by lazy { findViewById<ImageView>(R.id.alert_slider_icon)!! }
     private val textView by lazy { findViewById<TextView>(R.id.alert_slider_text)!! }
 
-    private val isLeft = context.resources.getBoolean(R.bool.alert_slider_dialog_left)
+    private val rotation: Int = context.getDisplay().getRotation()
+    private val isLandscape =
+        rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270
+    private val flip = !context.resources.getBoolean(R.bool.alert_slider_dialog_left)
+
+    private val length: Int
+    private val xPos: Int
+    private val yPos: Int
 
     init {
         window?.let {
@@ -56,49 +65,60 @@ class AlertSliderDialog(private val context: Context)
 
         setCanceledOnTouchOutside(false)
         setContentView(R.layout.alert_slider_dialog)
+
+        // position calculations
+        val res = context.resources
+        val fraction = res.getFraction(R.fraction.alert_slider_dialog_y, 1, 1)
+        val widthPixels = res.displayMetrics.widthPixels
+        val heightPixels = res.displayMetrics.heightPixels
+        val pads = dialogView.paddingTop * 2 // equal paddings in all 4 directions
+        length = if (isLandscape) res.getDimension(R.dimen.alert_slider_dialog_width).toInt()
+                 else res.getDimension(R.dimen.alert_slider_dialog_height).toInt()
+        val hv = (length + pads) * 0.5
+
+        xPos = if (isLandscape) (widthPixels * fraction - hv).toInt()
+               else if (flip) 0 else widthPixels / 100
+        yPos = if (isLandscape) (if (flip) (widthPixels / 100) else 0)
+               else (heightPixels * fraction - hv).toInt()
+
+        window?.let {
+            it.attributes = it.attributes.apply {
+                gravity = when (rotation) {
+                    Surface.ROTATION_0 ->
+                        if (flip) Gravity.TOP or Gravity.LEFT
+                        else Gravity.TOP or Gravity.RIGHT
+                    Surface.ROTATION_90 ->
+                        if (flip) Gravity.BOTTOM or Gravity.LEFT
+                        else Gravity.TOP or Gravity.LEFT
+                    Surface.ROTATION_270 ->
+                        if (flip) Gravity.TOP or Gravity.RIGHT
+                        else Gravity.BOTTOM or Gravity.RIGHT
+                    else ->
+                        if (flip) Gravity.BOTTOM or Gravity.LEFT
+                        else Gravity.TOP or Gravity.LEFT
+                }
+
+                x = xPos
+                y = yPos
+            }
+        }
     }
 
     fun setState(position: Int, ringerMode: Int) {
         window?.let {
             it.attributes = it.attributes.apply {
-                gravity = if (isLeft) {
-                    Gravity.TOP or Gravity.LEFT
-                } else {
-                    Gravity.TOP or Gravity.RIGHT
+                val delta = length * when (position) {
+                    KeyHandler.POSITION_TOP -> -1
+                    KeyHandler.POSITION_BOTTOM -> 1
+                    else -> 0 // KeyHandler.POSITION_MIDDLE
                 }
 
-                val f = context.resources.getFraction(R.fraction.alert_slider_dialog_y, 1, 1)
-                val h = context.resources.getDimension(R.dimen.alert_slider_dialog_height).toInt()
-                val hv = h + dialogView.paddingTop + dialogView.paddingBottom
-
-                x = context.resources.displayMetrics.widthPixels / 100
-                y = ((context.resources.displayMetrics.heightPixels * f) - (hv * 0.5)).toInt()
-
-                when (position) {
-                    KeyHandler.POSITION_TOP -> {
-                        y -= (h * 1.5).toInt()
-                    }
-                    KeyHandler.POSITION_BOTTOM -> {
-                        y += (h * 1.5).toInt()
-                    }
-                    else -> {}
-                }
+                if (isLandscape) x = xPos + delta
+                else y = yPos + delta
             }
         }
 
-        frameView.setBackgroundResource(when (position) {
-            KeyHandler.POSITION_TOP -> if (isLeft) {
-                R.drawable.alert_slider_top_flip
-            } else {
-                R.drawable.alert_slider_top
-            }
-            KeyHandler.POSITION_MIDDLE -> R.drawable.alert_slider_middle
-            else -> if (isLeft) {
-                R.drawable.alert_slider_bottom_flip
-            } else {
-                R.drawable.alert_slider_bottom
-            }
-        })
+        frameView.setBackgroundResource(backgroundFor(rotation, position, flip))
 
         iconView.setImageResource(when (ringerMode) {
             AudioManager.RINGER_MODE_SILENT -> R.drawable.ic_volume_ringer_mute
@@ -119,6 +139,41 @@ class AlertSliderDialog(private val context: Context)
             KeyHandler.ZEN_ALARMS_ONLY -> R.string.alert_slider_mode_dnd_alarms_only
             else -> R.string.alert_slider_mode_none
         })
+    }
+
+    private fun backgroundFor(rotation: Int, position: Int, flip: Boolean): Int {
+        fun base(position: Int): Int = when (position) {
+            KeyHandler.POSITION_TOP ->
+                if (flip) R.drawable.alert_slider_top_flip
+                else R.drawable.alert_slider_top
+            KeyHandler.POSITION_MIDDLE -> R.drawable.alert_slider_middle
+            KeyHandler.POSITION_BOTTOM ->
+                if (flip) R.drawable.alert_slider_bottom_flip
+                else R.drawable.alert_slider_bottom
+            else -> R.drawable.alert_slider_middle
+        }
+
+        return when (rotation) {
+            Surface.ROTATION_90 -> when (position) {
+                KeyHandler.POSITION_TOP ->
+                    if (flip) R.drawable.alert_slider_top_90_flip
+                    else R.drawable.alert_slider_top_90
+                KeyHandler.POSITION_BOTTOM ->
+                    if (flip) R.drawable.alert_slider_bottom_90_flip
+                    else R.drawable.alert_slider_bottom_90
+                else -> R.drawable.alert_slider_middle
+            }
+            Surface.ROTATION_270 -> when (position) {
+                KeyHandler.POSITION_TOP ->
+                    if (flip) R.drawable.alert_slider_top_270_flip
+                    else R.drawable.alert_slider_top_270
+                KeyHandler.POSITION_BOTTOM ->
+                    if (flip) R.drawable.alert_slider_bottom_270_flip
+                    else R.drawable.alert_slider_bottom_270
+                else -> R.drawable.alert_slider_middle
+            }
+            else -> base(position) // ROTATION_0 / ROTATION_180
+        }
     }
 
     companion object {
