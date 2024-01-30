339fc1ff09dbfdec6f5899fcc3b6d88d57f0a3bf
LuK1337 <priv.luk@gmail.com>
2024-01-30 09:56:20 +0100

    Add inherit makefiles for libqti-perfd-client/power-libperfmgr
    Also, hide libqti-perfd-client behind a soong namespace to avoid warning
    on devices that don't use it.
    
    Change-Id: I9d37e9d19b1a6f951a11a9a22eca8c0cf4423dce

---
+++ libqti-perfd-client/libqti-perfd-client.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
