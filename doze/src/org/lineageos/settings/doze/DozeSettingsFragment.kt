884a7584ca9398e36832ac01611efe654109c9e9
LuK1337 <priv.luk@gmail.com>
2024-03-11 16:15:20 +0100

    doze: Fix build on QPR2
    Change-Id: I7b1486116c89f7df89753ba1ac2493c7bbb9cf95

--- doze/src/org/lineageos/settings/doze/DozeSettingsFragment.kt
+++ doze/src/org/lineageos/settings/doze/DozeSettingsFragment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -50,13 +50,13 @@ class DozeSettingsFragment : PreferenceFragment(), Preference.OnPreferenceChange
         alwaysOnDisplayPreference.onPreferenceChangeListener = this
 
         val pickupSensorCategory =
-            preferenceScreen.findPreference<PreferenceCategory>(Utils.CATEGORY_PICKUP_SENSOR)
+            preferenceScreen.findPreference<PreferenceCategory>(Utils.CATEGORY_PICKUP_SENSOR)!!
         if (getString(R.string.pickup_sensor_type).isEmpty()) {
             preferenceScreen.removePreference(pickupSensorCategory)
         }
 
         val proximitySensorCategory =
-            preferenceScreen.findPreference<PreferenceCategory>(Utils.CATEGORY_PROXIMITY_SENSOR)
+            preferenceScreen.findPreference<PreferenceCategory>(Utils.CATEGORY_PROXIMITY_SENSOR)!!
         if (getString(R.string.pocket_sensor_type).isEmpty()) {
             preferenceScreen.removePreference(proximitySensorCategory)
         }
@@ -73,8 +73,8 @@ class DozeSettingsFragment : PreferenceFragment(), Preference.OnPreferenceChange
         if (!Utils.alwaysOnDisplayAvailable(context)) {
             preferenceScreen.removePreference(alwaysOnDisplayPreference)
         } else {
-            pickupSensorCategory?.dependency = Utils.ALWAYS_ON_DISPLAY
-            proximitySensorCategory?.dependency = Utils.ALWAYS_ON_DISPLAY
+            pickupSensorCategory.dependency = Utils.ALWAYS_ON_DISPLAY
+            proximitySensorCategory.dependency = Utils.ALWAYS_ON_DISPLAY
         }
     }
 
