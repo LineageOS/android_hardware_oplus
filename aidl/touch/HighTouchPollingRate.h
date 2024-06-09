aa8ebada0fac0cfca8f96d8640ed867e0afc2506
Maitreya25 <maitreyapatni25@gmail.com>
2024-06-09 20:06:00 +0530

    touch: Support interfacing with stock OplusTouch service
    Co-authored-by: inferno0230 <mail@inferno0230.in>
    Co-authored-by: Mashopy <eliasgheeraert@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: I305ac931dac35a7fa422745f4250ffb1145c3bf6

--- aidl/touch/HighTouchPollingRate.h
+++ aidl/touch/HighTouchPollingRate.h
@@ -6,16 +6,24 @@
 #pragma once
 
 #include <aidl/vendor/lineage/touch/BnHighTouchPollingRate.h>
+#include <aidl/vendor/oplus/hardware/touch/IOplusTouch.h>
 
 namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace touch {
 
+using aidl::vendor::oplus::hardware::touch::IOplusTouch;
+
 class HighTouchPollingRate : public BnHighTouchPollingRate {
   public:
+    explicit HighTouchPollingRate(std::shared_ptr<IOplusTouch> oplusTouch);
+
     ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
     ndk::ScopedAStatus setEnabled(bool enabled) override;
+
+  private:
+    std::shared_ptr<IOplusTouch> mOplusTouch;
 };
 
 }  // namespace touch
