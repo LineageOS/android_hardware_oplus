6de1ec6ac6724d5b4180ac292ae4d76c56a4d0b7
Maitreya25 <maitreyapatni25@gmail.com>
2024-06-09 20:06:00 +0530

    touch: Implement TouchscreenGesture and HighTouchPollingRate through OplusTouch service
    [inferno0230] Implement HighTouchPollingRate through OplusTouch
    [Mashopy] Introduce ENABLE_OPLUSTOUCH soong boolean flag
    [Mashopy] Fixup HighTouchPollingRate isEnabled return logic for OplusTouch
    [Mashopy] Write 1 instead of the game param value to enable the game_switch node for OplusTouch
    [Mashopy] Update to vendor.oplus.hardware.touch-V2-ndk
    
    Co-authored-by: inferno0230 <mail@inferno0230.in>
    Co-authored-by: Mashopy <eliasgheeraert@gmail.com>
    Change-Id: I305ac931dac35a7fa422745f4250ffb1145c3bf6

--- hidl/touch/HighTouchPollingRate.cpp
+++ hidl/touch/HighTouchPollingRate.cpp
@@ -20,12 +20,21 @@
 
 #include <android-base/file.h>
 
+#ifdef ENABLE_OPLUSTOUCH
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#endif
+
+#ifndef ENABLE_OPLUSTOUCH
 using ::android::base::ReadFileToString;
 using ::android::base::WriteStringToFile;
+#endif
 
 namespace {
 
+#ifndef ENABLE_OPLUSTOUCH
 constexpr const char* kGameSwitchEnablePath = "/proc/touchpanel/game_switch_enable";
+#endif
 
 }  // anonymous namespace
 
@@ -37,11 +46,37 @@ namespace implementation {
 
 Return<bool> HighTouchPollingRate::isEnabled() {
     std::string value;
+#ifdef ENABLE_OPLUSTOUCH
+    // Connect to IOplusTouch
+    const std::string instance = std::string() + IOplusTouch::descriptor + "/default";
+    mTouchService = IOplusTouch::fromBinder(
+            ndk::SpAIBinder(AServiceManager_waitForService(instance.c_str())));
+
+    // Read current value
+    mTouchService->touchReadNodeFile(0, 26, &value);
+
+    return value[0] != '0';
+#else
     return ReadFileToString(kGameSwitchEnablePath, &value) && value[0] != '0';
+#endif
 }
 
 Return<bool> HighTouchPollingRate::setEnabled(bool enabled) {
+#ifdef ENABLE_OPLUSTOUCH
+    int result;
+
+    // Connect to IOplusTouch
+    const std::string instance = std::string() + IOplusTouch::descriptor + "/default";
+    mTouchService = IOplusTouch::fromBinder(
+            ndk::SpAIBinder(AServiceManager_waitForService(instance.c_str())));
+
+    // Always keep gestures enabled
+    mTouchService->touchWriteNodeFile(0, 26, enabled ? "1" : "0", &result);
+
+    return true;
+#else
     return WriteStringToFile(enabled ? "1" : "0", kGameSwitchEnablePath, true);
+#endif
 }
 
 }  // namespace implementation
