910f487c93198e6d47d19fc179a2c12755b0f6cb
Maitreya25 <maitreyapatni25@gmail.com>
2024-06-09 20:06:00 +0530

    touch: Support interfacing with stock OplusTouch service
    Co-authored-by: inferno0230 <mail@inferno0230.in>
    Co-authored-by: Mashopy <eliasgheeraert@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: I305ac931dac35a7fa422745f4250ffb1145c3bf6

--- aidl/touch/TouchscreenGesture.cpp
+++ aidl/touch/TouchscreenGesture.cpp
@@ -25,6 +25,9 @@ namespace vendor {
 namespace lineage {
 namespace touch {
 
+TouchscreenGesture::TouchscreenGesture(std::shared_ptr<IOplusTouch> oplusTouch)
+    : mOplusTouch(std::move(oplusTouch)) {}
+
 ndk::ScopedAStatus TouchscreenGesture::getSupportedGestures(std::vector<Gesture>* _aidl_return) {
     std::vector<Gesture> gestures;
 
@@ -39,9 +42,13 @@ ndk::ScopedAStatus TouchscreenGesture::getSupportedGestures(std::vector<Gesture>
 }
 
 ndk::ScopedAStatus TouchscreenGesture::setGestureEnabled(const Gesture& gesture, bool enabled) {
+    std::string tmp;
     int contents = 0;
 
-    if (std::string tmp; ReadFileToString(kGestureEnableIndepPath, &tmp)) {
+    if (mOplusTouch) {
+        mOplusTouch->touchReadNodeFile(0, 21, &tmp);
+        contents = std::stoi(tmp, nullptr, 16);
+    } else if (ReadFileToString(kGestureEnableIndepPath, &tmp)) {
         contents = std::stoi(Trim(tmp), nullptr, 16);
     } else {
         return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
@@ -53,7 +60,10 @@ ndk::ScopedAStatus TouchscreenGesture::setGestureEnabled(const Gesture& gesture,
         contents &= ~(1 << (gesture.keycode - kGestureStartKey));
     }
 
-    if (!WriteStringToFile(std::to_string(contents), kGestureEnableIndepPath, true)) {
+    if (mOplusTouch) {
+        mOplusTouch->touchWriteNodeFileOneWay(0, 1, "1");
+        mOplusTouch->touchWriteNodeFileOneWay(0, 21, std::to_string(contents));
+    } else if (!WriteStringToFile(std::to_string(contents), kGestureEnableIndepPath, true)) {
         return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
     }
 
