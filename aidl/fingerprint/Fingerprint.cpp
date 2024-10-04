a56965e0aa94a2f79ad4c9f9da553b2588f3b72f
Bruno Martins <bgcngm@gmail.com>
2024-10-04 21:59:01 +0100

    aidl: fingerprint: Make it act as aidl proxy
    Change-Id: Ib75614c48fd175f712b0505af0539e56bfe11ae1

--- aidl/fingerprint/Fingerprint.cpp
+++ aidl/fingerprint/Fingerprint.cpp
@@ -6,10 +6,79 @@
 
 #include "Fingerprint.h"
 
-namespace aidl::android::hardware::biometrics::fingerprint {
+#include <android-base/logging.h>
+#include <android-base/parseint.h>
+#include <android-base/strings.h>
 
-ndk::ScopedAStatus Fingerprint::getSensorProps(std::vector<SensorProps>* /*out*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+#include <fingerprint.sysprop.h>
+
+using ::android::base::ParseInt;
+using ::android::base::Split;
+
+namespace aidl::android::hardware::biometrics::fingerprint {
+namespace {
+constexpr int SENSOR_ID = 0;
+constexpr common::SensorStrength SENSOR_STRENGTH = common::SensorStrength::STRONG;
+constexpr int MAX_ENROLLMENTS_PER_USER = 4;
+constexpr char HW_COMPONENT_ID[] = "fingerprintSensor";
+constexpr char HW_VERSION[] = "vendor/model/revision";
+constexpr char FW_VERSION[] = "1.0";
+constexpr char SERIAL_NUMBER[] = "00000001";
+constexpr char SW_COMPONENT_ID[] = "matchingAlgorithm";
+constexpr char SW_VERSION[] = "vendor/version/revision";
+}
+
+Fingerprint::Fingerprint() {
+    std::string sensorTypeProp = FingerprintHalProperties::type().value_or("");
+    if (sensorTypeProp == "" || sensorTypeProp == "default" || sensorTypeProp == "rear")
+        mSensorType = FingerprintSensorType::REAR;
+    else if (sensorTypeProp == "udfps")
+        mSensorType = FingerprintSensorType::UNDER_DISPLAY_ULTRASONIC;
+    else if (sensorTypeProp == "udfps_optical")
+        mSensorType = FingerprintSensorType::UNDER_DISPLAY_OPTICAL;
+    else if (sensorTypeProp == "side")
+        mSensorType = FingerprintSensorType::POWER_BUTTON;
+    else if (sensorTypeProp == "home")
+        mSensorType = FingerprintSensorType::HOME_BUTTON;
+    else
+        mSensorType = FingerprintSensorType::UNKNOWN;
+
+    mMaxEnrollmentsPerUser =
+            FingerprintHalProperties::max_enrollments_per_user().value_or(MAX_ENROLLMENTS_PER_USER);
+
+    return;
+}
+
+ndk::ScopedAStatus Fingerprint::getSensorProps(std::vector<SensorProps>* out) {
+    std::vector<common::ComponentInfo> componentInfo = {
+            {HW_COMPONENT_ID, HW_VERSION, FW_VERSION, SERIAL_NUMBER, "" /* softwareVersion */},
+            {SW_COMPONENT_ID, "" /* hardwareVersion */, "" /* firmwareVersion */,
+            "" /* serialNumber */, SW_VERSION}};
+    common::CommonProps commonProps = {SENSOR_ID, SENSOR_STRENGTH,
+                                       mMaxEnrollmentsPerUser, componentInfo};
+    SensorLocation sensorLocation;
+    std::string loc = FingerprintHalProperties::sensor_location().value_or("");
+    std::vector<std::string> dim = Split(loc, "|");
+    if (dim.size() >= 3 && dim.size() <= 4) {
+        ParseInt(dim[0], &sensorLocation.sensorLocationX);
+        ParseInt(dim[1], &sensorLocation.sensorLocationY);
+        ParseInt(dim[2], &sensorLocation.sensorRadius);
+        if (dim.size() >= 4)
+            sensorLocation.display = dim[3];
+    } else if(loc.length() > 0) {
+        LOG(WARNING) << "Invalid sensor location input (x|y|radius|display): " << loc;
+    }
+    LOG(INFO) << "Sensor type: " << ::android::internal::ToString(mSensorType)
+              << " location: " << sensorLocation.toString();
+    *out = {{commonProps,
+             mSensorType,
+             {sensorLocation},
+             false,
+             false,
+             false,
+             false,
+             std::nullopt}};
+    return ndk::ScopedAStatus::ok();
 }
 
 ndk::ScopedAStatus Fingerprint::createSession(int32_t /*sensorId*/, int32_t /*userId*/,
