a1c23d194c395c89964d5d47779cf3b09da0c5a2
John Galt <johngaltfirstrun@gmail.com>
2024-10-11 11:58:35 -0400

    osense: add secondary fqname
    Fingerprint service for some devices has only one default interface
    initialization as "default" which we patch to "oplus" for the proxy.
    
    It then tries to load everything it depends on (touch and osense) using
    "oplus" fqname instead of "default".
    
    Change-Id: I7cd24b6f556372b2c5a3588abef2c197b212822d

--- aidl/osense-client/service.cpp
+++ aidl/osense-client/service.cpp
@@ -18,9 +18,13 @@ int main() {
             ndk::SharedRefBase::make<OsenseAidlHalReporter>();
 
     const std::string instance = std::string() + OsenseAidlHalReporter::descriptor + "/default";
+    const std::string instance2 = std::string() + OsenseAidlHalReporter::descriptor + "/oplus";
     binder_status_t status =
             AServiceManager_addService(reporter->asBinder().get(), instance.c_str());
     CHECK_EQ(status, STATUS_OK);
+    binder_status_t status2 =
+            AServiceManager_addService(reporter->asBinder().get(), instance2.c_str());
+    CHECK_EQ(status2, STATUS_OK);
 
     ABinderProcess_joinThreadPool();
     return EXIT_FAILURE;  // should not reach
