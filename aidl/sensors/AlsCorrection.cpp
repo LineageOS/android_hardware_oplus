0e834b378bf7f77012c684fe949a68518578c37e
pjgowtham <pjgowtham@gmail.com>
2024-12-20 18:27:47 +0530

    aidl: sensors: Return early if nothing is displayed over the light sensor
    Change-Id: I65359c9d43448a306b215d7340ada15e2f2eec2a

--- aidl/sensors/AlsCorrection.cpp
+++ aidl/sensors/AlsCorrection.cpp
@@ -207,6 +207,11 @@ void AlsCorrection::process(Event& event) {
             event.sensorHandle = 0;
             return;
         }
+
+        if (screenshot.r + screenshot.g + screenshot.b == 0) {
+            return;
+        }
+
         ALOGV("Screen color above sensor: %f %f %f", screenshot.r, screenshot.g, screenshot.b);
 
         float rgbw[4] = {
