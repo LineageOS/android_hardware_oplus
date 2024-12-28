1219c1e12141c46eb6be6b61b03ec91e48427b28
raghavt20 <raghavt20@aospa.co>
2024-12-28 09:22:16 +0000

    vibrator: qti: Add support for cliffs (SM8635)
    Change-Id: Iae1c0fbe5fa65f1978e0e1ee7bebca99cf830b3b
    Signed-off-by: grepfox <grepfox@tutamail.com>

--- aidl/vibrator/qti/Vibrator.cpp
+++ aidl/vibrator/qti/Vibrator.cpp
@@ -69,6 +69,7 @@ namespace vibrator {
 #define MSM_CPU_KALAMA          519
 #define MSM_CPU_PINEAPPLE       557
 #define MSM_CPU_SUN             618
+#define MSM_CPU_SM8635          614
 
 #define test_bit(bit, array)    ((array)[(bit)/8] & (1<<((bit)%8)))
 
@@ -158,6 +159,7 @@ InputFFDevice::InputFFDevice()
             case MSM_CPU_KALAMA:
             case MSM_CPU_PINEAPPLE:
             case MSM_CPU_SUN:
+            case MSM_CPU_SM8635:
                 mSupportExternalControl = true;
                 break;
             default:
