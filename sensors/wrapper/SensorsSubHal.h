a19bd2328a3cbd2997cfac6cec170cb8474e95da
dianlujitao <dianlujitao@lineageos.org>
2025-02-07 15:21:01 +0800

    sensors: Add a wrapper for sensors HAL
    Change-Id: I2c2f482b7c6b3f7baad1ae2d42e9c5bba1647269

---
+++ sensors/wrapper/SensorsSubHal.h
@@ -0,0 +1,60 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <V2_1/SubHal.h>
+
+namespace android {
+namespace hardware {
+namespace sensors {
+namespace V2_1 {
+namespace subhal {
+namespace implementation {
+namespace wrapper {
+
+using ::android::hardware::sensors::V1_0::OperationMode;
+using ::android::hardware::sensors::V1_0::RateLevel;
+using ::android::hardware::sensors::V1_0::Result;
+using ::android::hardware::sensors::V1_0::SharedMemInfo;
+using ::android::hardware::sensors::V2_1::Event;
+using ::android::hardware::sensors::V2_1::implementation::IHalProxyCallback;
+using ::android::hardware::sensors::V2_1::implementation::ISensorsSubHal;
+
+class SensorsSubHal : public ISensorsSubHal {
+  public:
+    SensorsSubHal();
+
+    // ISensors
+    Return<Result> setOperationMode(OperationMode mode) override;
+    Return<Result> activate(int32_t sensor_handle, bool enabled) override;
+    Return<Result> batch(int32_t sensor_handle, int64_t sampling_period_ns,
+                         int64_t max_report_latency_ns) override;
+    Return<Result> flush(int32_t sensor_handle) override;
+    Return<void> registerDirectChannel(const SharedMemInfo& mem,
+                                       ISensors::registerDirectChannel_cb _hidl_cb) override;
+    Return<Result> unregisterDirectChannel(int32_t channel_handle) override;
+    Return<void> configDirectReport(int32_t sensor_handle, int32_t channel_handle, RateLevel rate,
+                                    ISensors::configDirectReport_cb _hidl_cb) override;
+    Return<void> getSensorsList_2_1(ISensors::getSensorsList_2_1_cb _hidl_cb) override;
+    Return<Result> injectSensorData_2_1(const Event& event) override;
+
+    // ISensorsSubHal
+    Return<void> debug(const hidl_handle& fd, const hidl_vec<hidl_string>& args) override;
+    const std::string getName() override;
+    Return<Result> initialize(const sp<IHalProxyCallback>& hal_proxy_callback) override;
+
+  private:
+    std::unique_ptr<void, std::function<void(void*)>> lib_handle_;
+    V2_1::implementation::ISensorsSubHal* impl_;
+};
+
+}  // namespace wrapper
+}  // namespace implementation
+}  // namespace subhal
+}  // namespace V2_1
+}  // namespace sensors
+}  // namespace hardware
+}  // namespace android
