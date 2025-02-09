8009823447512aa8633262b96f418f3008f2aa45
Bruno Martins <bgcngm@gmail.com>
2025-02-09 23:22:17 +0000

    Introduce vendor.oplus.hardware.commondcs-service
    Change-Id: I4d91094cc2274a168b147a53d5ebe412a867e920

---
+++ aidl/commondcs/CommonDcsAidlHalService.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "CommonDcsAidlHalService.h"
+
+namespace aidl {
+namespace vendor {
+namespace oplus {
+namespace hardware {
+namespace commondcs {
+
+ndk::ScopedAStatus CommonDcsAidlHalService::notifyMsgToCommonDcs(
+        const std::vector<::aidl::vendor::oplus::hardware::commondcs::StringPair>& data,
+        const std::string& logTag, const std::string& eventId, int32_t* _aidl_return) {
+    *_aidl_return = 1;
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace commondcs
+}  // namespace hardware
+}  // namespace oplus
+}  // namespace vendor
+}  // namespace aidl
