9564fa90ac50b78ea1e6b4f1af28d1005c3d160d
dianlujitao <dianlujitao@lineageos.org>
2025-02-11 14:00:12 +0800

    Add ALS capture service
    Change-Id: I86c20e432743ac909def170008fd13a9e50436f8

---
+++ sepolicy/common/SEPolicy.mk
@@ -0,0 +1,13 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/common/vendor
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/common/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/common/public
