e53172877bd09a4bd9c18c77abf2984901b599e0
dianlujitao <dianlujitao@lineageos.org>
2025-02-11 14:00:12 +0800

    Add ALS capture service
    Imported from oneplus_sm8150-common
    
    Co-authored-by: Georg Veichtlbauer <georg@vware.at>
    Change-Id: I86c20e432743ac909def170008fd13a9e50436f8

---
+++ sepolicy/common/SEPolicy.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/common/vendor
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/common/private
