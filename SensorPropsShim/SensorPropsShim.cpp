32069da2f11b967d302ee2aff12ab07eb87d4bce
LuK1337 <priv.luk@gmail.com>
2025-02-13 08:34:39 +0100

    aaaaaa
    Change-Id: Ia25e0c007af845c14fb98cb201bac334d70a74a4

---
+++ SensorPropsShim/SensorPropsShim.cpp
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <aidl/android/hardware/biometrics/fingerprint/SensorProps.h>
+#include <dlfcn.h>
+
+using SensorProps = aidl::android::hardware::biometrics::fingerprint::SensorProps;
+
+SensorProps sensorProps;
+
+extern "C" void
+_ZNK4aidl7android8hardware10biometrics11fingerprint11SensorProps13writeToParcelEP7AParcel(
+        SensorProps* thisptr, AParcel* parcel) {
+    static auto writeToParcel = reinterpret_cast<
+            typeof(_ZNK4aidl7android8hardware10biometrics11fingerprint11SensorProps13writeToParcelEP7AParcel)*>(
+            dlsym(RTLD_NEXT, __func__));
+    writeToParcel(&sensorProps, parcel);
+}
+
+__attribute__((constructor)) void SensorPropsInit() {
+    // customize everything here
+    sensorProps.supportsDetectInteraction = true;
+}
