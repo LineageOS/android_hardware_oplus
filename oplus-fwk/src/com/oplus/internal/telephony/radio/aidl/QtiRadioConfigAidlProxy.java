21f9d2be2a4e51fbc7a13423f6cc1a88e6403d28
Bruno Martins <bgcngm@gmail.com>
2025-02-17 21:59:24 +0000

    oplus-fwk: Introduce QtiRadioConfigAidlProxy
    Change-Id: Id759fb74a2fcf037774f076a79c16590761a1b2d

---
+++ oplus-fwk/src/com/oplus/internal/telephony/radio/aidl/QtiRadioConfigAidlProxy.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.oplus.internal.telephony.radio.aidl;
+
+import android.content.Context;
+import android.telephony.Rlog;
+
+public class QtiRadioConfigAidlProxy {
+    private int mCurrentAidlVersion;
+
+    public static QtiRadioConfigAidlProxy sInstance = null;
+
+    public QtiRadioConfigAidlProxy(Context context) {
+        mCurrentAidlVersion = -1;
+    }
+
+    public static QtiRadioConfigAidlProxy getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new QtiRadioConfigAidlProxy(context);
+        }
+        return sInstance;
+    }
+
+    public boolean isFeatureSupported(int i) {
+        if (i != 101 || mCurrentAidlVersion < 5) {
+            return false;
+        }
+        Rlog.d("QtiRadioConfigAidl", "FEATURE_SMART_TEMP_DDS_VIA_RADIO_CONFIG feature supported");
+        return true;
+    }
+}
