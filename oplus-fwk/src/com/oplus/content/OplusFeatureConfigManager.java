6eeb70c94f7bc8adc3cfd081c733e69d96767adf
pjgowtham <pjgowtham@gmail.com>
2025-02-28 18:04:30 +0530

    oplus-fwk: Update for compatibility with android-15 IMS stack
    Change-Id: Ib783e8a7653903cea0e2afa7596d6a762342e781

--- oplus-fwk/src/com/oplus/content/OplusFeatureConfigManager.java
+++ oplus-fwk/src/com/oplus/content/OplusFeatureConfigManager.java
@@ -26,4 +26,8 @@ public class OplusFeatureConfigManager {
     public interface OnFeatureObserver {
         default void onFeatureUpdate(List<String> features) {}
     }
+
+    public boolean hasFeature(String name) {
+        return false;
+    }
 }
