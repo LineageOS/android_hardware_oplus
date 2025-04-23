0d1e4c2799d08a6aad473a1624422dd1051b1274
Bruno Martins <bgcngm@gmail.com>
2025-04-23 22:15:37 +0100

    powershare: Migrate to AIDL
    Change-Id: I181478469e0de7e913a8e3fb665155b63a74f63a

---
+++ aidl/powershare/service.cpp
@@ -0,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.powershare-service.oplus"
+
+#include "PowerShare.h"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+using aidl::vendor::lineage::powershare::PowerShare;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<PowerShare> service = ndk::SharedRefBase::make<PowerShare>();
+
+    const std::string instance = std::string(PowerShare::descriptor) + "/default";
+    binder_status_t status =
+            AServiceManager_addService(service->asBinder().get(), instance.c_str());
+
+    if (status != STATUS_OK) {
+        LOG(ERROR) << "Can't register PowerShare HAL service";
+        return 1;
+    }
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}
