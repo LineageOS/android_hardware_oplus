0579a76e76478c7944f75672d529519259f22409
LuK1337 <priv.luk@gmail.com>
2025-06-09 13:15:47 +0200

    touch: Introduce vendor.oplus.hardware.touch-headers
    Change-Id: If93b1305f7ee82f2c954969f83c052d0b3a2772b

---
+++ aidl/touch/touch-headers/OplusTouchConstants.h
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+// Device IDs
+static constexpr int DEFAULT_TP_IC_ID = 0;
+static constexpr int SUB_DISPLAY_TP_IC_ID = 1;
+
+// Node IDs
+static constexpr int DOUBLE_TAP_ENABLE_NODE = 1;
+static constexpr int DOUBLE_TAP_INDEP_NODE = 21;
+static constexpr int GAME_SWITCH_ENABLE_NODE = 26;
