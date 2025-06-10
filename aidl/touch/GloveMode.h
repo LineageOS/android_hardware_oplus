c2ff46186752df534c1caaf56e09f28e625aedec
Bruno Martins <bgcngm@gmail.com>
2025-06-10 19:38:24 +0100

    touch: Implement GloveMode support
    Change-Id: I1bb2f502e5b6947dfb2d68572b098d0e5b058c28

---
+++ aidl/touch/GloveMode.h
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/touch/BnGloveMode.h>
+#include <aidl/vendor/oplus/hardware/touch/IOplusTouch.h>
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace touch {
+
+using aidl::vendor::oplus::hardware::touch::IOplusTouch;
+
+class GloveMode : public BnGloveMode {
+  public:
+    explicit GloveMode(std::shared_ptr<IOplusTouch> oplusTouch);
+
+    ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool enabled) override;
+
+  private:
+    std::shared_ptr<IOplusTouch> mOplusTouch;
+};
+
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
