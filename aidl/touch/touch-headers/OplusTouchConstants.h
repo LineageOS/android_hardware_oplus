e59653be066ff57d4eb2d8fd66cded0858dae447
Bruno Martins <bgcngm@gmail.com>
2025-06-10 19:38:24 +0100

    touch: Implement GloveMode support
    Change-Id: I1bb2f502e5b6947dfb2d68572b098d0e5b058c28

--- aidl/touch/touch-headers/OplusTouchConstants.h
+++ aidl/touch/touch-headers/OplusTouchConstants.h
@@ -12,11 +12,12 @@ constexpr int DEFAULT_TP_IC_ID = 0;
 constexpr int SUB_DISPLAY_TP_IC_ID = 1;
 
 // Features
-static constexpr int DOUBLE_TAP_GESTURE = 1 << 1;
+constexpr int DOUBLE_TAP_GESTURE = 1 << 1;
 
 // Node IDs
 constexpr int DOUBLE_TAP_ENABLE_NODE = 1;
 constexpr int DOUBLE_TAP_INDEP_NODE = 21;
 constexpr int GAME_SWITCH_ENABLE_NODE = 26;
+constexpr int GLOVE_MODE_ENABLE_NODE = 192;
 
 }  // namespace OplusTouchConstants
