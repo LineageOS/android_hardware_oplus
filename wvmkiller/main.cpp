639ede8b3bc0f6561f680001efc9121118de4813
LuK1337 <priv.luk@gmail.com>
2025-07-20 20:49:19 +0200

    wvmkiller: Add support for AIDL
    Change-Id: Ic15cb1708b3708a708bcf43d81330a8e05c30643

--- wvmkiller/main.cpp
+++ wvmkiller/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 The LineageOS Project
+ * Copyright (C) 2024-2025 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,39 +14,78 @@
  * limitations under the License.
  */
 
+#include <aidl/android/hardware/drm/IDrmFactory.h>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
+#include <android/binder_manager.h>
 #include <android/hardware/drm/1.0/IDrmFactory.h>
 
 using android::sp;
 using android::base::SetProperty;
-using android::hardware::hidl_array;
-using android::hardware::hidl_string;
-using android::hardware::drm::V1_0::IDrmFactory;
-using android::hardware::drm::V1_0::IDrmPlugin;
-using android::hardware::drm::V1_0::Status;
 
-int main() {
-    const uint8_t WIDEVINE_UUID[] = {0xED, 0xEF, 0x8B, 0xA9, 0x79, 0xD6, 0x4A, 0xCE,
-                                     0xA3, 0xC8, 0x27, 0xDC, 0xD5, 0x1D, 0x21, 0xED};
+const char APP_PACKAGE_NAME[] = "org.lineageos.wvmkiller";
+const uint8_t WIDEVINE_UUID[] = {0xED, 0xEF, 0x8B, 0xA9, 0x79, 0xD6, 0x4A, 0xCE,
+                                 0xA3, 0xC8, 0x27, 0xDC, 0xD5, 0x1D, 0x21, 0xED};
 
-    const auto callback = [](Status status, const sp<IDrmPlugin>& drmPlugin) {
+std::string systemIdHidl() {
+    using android::hardware::hidl_array;
+    using android::hardware::hidl_string;
+    using android::hardware::drm::V1_0::IDrmFactory;
+    using android::hardware::drm::V1_0::IDrmPlugin;
+    using android::hardware::drm::V1_0::Status;
+
+    std::string ret;
+
+    const auto callback = [&](Status status, const sp<IDrmPlugin>& drmPlugin) {
         CHECK(status == Status::OK);
 
-        drmPlugin->getPropertyString("systemId", [](Status status, const hidl_string& value) {
+        drmPlugin->getPropertyString("systemId", [&](Status status, const hidl_string& value) {
             CHECK(status == Status::OK);
-
-            if (value.size() > 5) {
-                SetProperty("vendor.wvm.disable_l1", "1");
-            }
+            ret = value;
         });
     };
 
     const auto drmFactory = IDrmFactory::getService("widevine");
-    CHECK(drmFactory.get() != nullptr);
 
-    drmFactory->createPlugin(hidl_array<uint8_t, 16>(WIDEVINE_UUID), "org.lineageos.wvmkiller",
-                             callback);
+    if (drmFactory) {
+        drmFactory->createPlugin(hidl_array<uint8_t, 16>(WIDEVINE_UUID), APP_PACKAGE_NAME,
+                                 callback);
+    }
+
+    return ret;
+}
+
+std::string systemIdAidl() {
+    using aidl::android::hardware::drm::IDrmFactory;
+    using aidl::android::hardware::drm::IDrmPlugin;
+    using aidl::android::hardware::drm::Uuid;
+
+    auto instance = std::format("{}/widevine", IDrmFactory::descriptor);
+
+    if (!AServiceManager_isDeclared(instance.c_str())) {
+        return "";
+    }
+
+    auto drmFactory =
+            IDrmFactory::fromBinder(ndk::SpAIBinder(AServiceManager_getService(instance.c_str())));
+    CHECK(drmFactory);
+
+    Uuid uuid;
+    uuid.uuid = std::to_array(WIDEVINE_UUID);
+
+    std::shared_ptr<IDrmPlugin> drmPlugin;
+    CHECK(drmFactory->createDrmPlugin(uuid, APP_PACKAGE_NAME, &drmPlugin).isOk());
+
+    std::string systemId;
+    CHECK(drmPlugin->getPropertyString("systemId", &systemId).isOk());
+
+    return systemId;
+}
+
+int main() {
+    if (systemIdAidl().size() > 5 || systemIdHidl().size() > 5) {
+        SetProperty("vendor.wvm.disable_l1", "1");
+    }
 
     return 0;
 }
