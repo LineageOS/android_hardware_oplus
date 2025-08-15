ed6287183b84f058a5862d6640cde95c29c316c1
LuK1337 <priv.luk@gmail.com>
2025-08-15 10:12:25 +0200

    livedisplay: Migrate to AIDL
    Change-Id: I8dbe9ed99549aff3e7fcff553482a4c45aae67ef

---
+++ aidl/livedisplay/AntiFlicker.cpp
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <fcntl.h>
+#include <livedisplay/oplus/AntiFlicker.h>
+#include <oplus/oplus_display_panel.h>
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+
+AntiFlicker::AntiFlicker() : mOplusDisplayFd(open("/dev/oplus_display", O_RDWR)) {}
+
+ndk::ScopedAStatus AntiFlicker::getEnabled(bool* _aidl_return) {
+    unsigned int value;
+    *_aidl_return =
+            ioctl(mOplusDisplayFd, PANEL_IOCTL_GET_DIMLAYER_BL_EN, &value) == 0 && value > 0;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus AntiFlicker::setEnabled(bool enabled) {
+    bool isEnabled;
+    getEnabled(&isEnabled);
+
+    if (isEnabled != enabled) {
+        unsigned int value = enabled;
+        ioctl(mOplusDisplayFd, PANEL_IOCTL_SET_DIMLAYER_BL_EN, &value);
+    }
+
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
