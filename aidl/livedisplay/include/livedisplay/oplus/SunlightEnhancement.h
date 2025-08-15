87ff07eb5b966592a51e0e2c32760f026d65619d
LuK1337 <priv.luk@gmail.com>
2025-08-15 10:12:25 +0200

    livedisplay: Migrate to AIDL
    Change-Id: I8dbe9ed99549aff3e7fcff553482a4c45aae67ef

---
+++ aidl/livedisplay/include/livedisplay/oplus/SunlightEnhancement.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2019-2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/livedisplay/BnSunlightEnhancement.h>
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+
+class SunlightEnhancement : public BnSunlightEnhancement {
+  public:
+    SunlightEnhancement();
+
+    // Methods from ::aidl::vendor::lineage::livedisplay::BnSunlightEnhancement follow.
+    ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool enabled, bool* _aidl_return) override;
+
+  private:
+    int mOplusDisplayFd;
+};
+
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
