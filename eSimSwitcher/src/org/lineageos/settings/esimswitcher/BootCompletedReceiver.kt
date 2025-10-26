401600cd39fe9b1ea316c117f15a926f53baabf2
Bruno Martins <bgcngm@gmail.com>
2025-10-26 14:35:06 +0000

    Introduce eSIM switcher app
    Icon taken from google/material-design-icons.
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: I28216910c39f4ee690d2e69d39158413422bfe8d

---
+++ eSimSwitcher/src/org/lineageos/settings/esimswitcher/BootCompletedReceiver.kt
@@ -0,0 +1,43 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.esimswitcher
+
+import android.content.BroadcastReceiver
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.telephony.TelephonyManager
+import android.util.Log
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Received boot completed intent")
+
+        val telephonyManager = context.getSystemService(TelephonyManager::class.java)
+        val hasNonRemovableEuicc = telephonyManager?.uiccSlotsInfo
+            ?.any { it.isEuicc && !it.isRemovable } ?: false
+
+        setComponentEnabled(context, EsimSettingsActivity::class.java.name, hasNonRemovableEuicc)
+    }
+
+    private fun setComponentEnabled(context: Context, component: String, enabled: Boolean) {
+        val name = ComponentName(context, component)
+        val pm = context.packageManager
+        val newState = if (enabled)
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+        else
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+
+        if (pm.getComponentEnabledSetting(name) != newState) {
+            pm.setComponentEnabledSetting(name, newState, PackageManager.DONT_KILL_APP)
+        }
+    }
+
+    companion object {
+        private const val TAG = "OplusEsimBootReceiver"
+    }
+}
