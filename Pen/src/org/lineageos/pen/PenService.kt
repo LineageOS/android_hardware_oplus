c2a153804253e33eebffe9ccac70d616edcaf4fd
LuK1337 <priv.luk@gmail.com>
2025-10-26 13:57:21 +0100

    Pen: Simplify stylus identification
    Change-Id: Ica177eab3a1b1e9fe4f63a3f2eda7afd5e44cd7e

--- Pen/src/org/lineageos/pen/PenService.kt
+++ Pen/src/org/lineageos/pen/PenService.kt
@@ -158,7 +158,14 @@ class PenService : Service() {
     private fun overridePeakRefreshRateIfNeeded() {
         val isPenConnected = inputManager.inputDeviceIds.firstOrNull {
             val device = inputManager.getInputDevice(it) ?: return@firstOrNull false
-            val deviceId = DeviceId.fromInputDevice(device) ?: return@firstOrNull false
+            if (device.vendorId != 0x22D9) {
+                // Not an OPPO vendor ID
+                return@firstOrNull false
+            }
+            if (device.bluetoothAddress?.startsWith("C0:87:06") == false) {
+                // Not a Maxeye prefix
+                return@firstOrNull false
+            }
             return@firstOrNull true
         } != null
         val peakRefreshRate = Settings.System.getString(contentResolver, PEAK_REFRESH_RATE)
