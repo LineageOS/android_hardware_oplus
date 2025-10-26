85f99a0f9f5f030cb3b0163ae83c12cca5105b9d
Bruno Martins <bgcngm@gmail.com>
2025-10-26 14:35:06 +0000

    Introduce eSIM switcher app
    Change-Id: I28216910c39f4ee690d2e69d39158413422bfe8d

---
+++ eSimSwitcher/src/org/lineageos/settings/esimswitcher/EsimSettingsFragment.kt
@@ -0,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.esimswitcher
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
+import org.lineageos.settings.esimswitcher.R
+
+class EsimSettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
+    private lateinit var controller: EsimController
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.esim_settings, rootKey)
+        controller = EsimController(requireContext())
+
+        val switcher = findPreference<SwitchPreferenceCompat>(ESIM_TOGGLE_KEY)
+        switcher?.isChecked = controller.currentGpioState() == 1
+        switcher?.onPreferenceChangeListener = this
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        val enable = newValue as Boolean
+        if (enable) {
+            AlertDialog.Builder(requireContext())
+                .setMessage(R.string.esim_toggle_dialog)
+                .setNegativeButton(R.string.esim_toggle_dialog_cancel) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setPositiveButton(R.string.esim_toggle_dialog_ok) { dialog, _ ->
+                    dialog.dismiss()
+                    controller.toggleEsimState(1)
+                    (preference as? SwitchPreferenceCompat)?.isChecked = true
+                }
+                .show()
+            return false
+        } else {
+            controller.toggleEsimState(0)
+            return true
+        }
+    }
+
+    companion object {
+        private const val ESIM_TOGGLE_KEY = "esim_toggle"
+    }
+}
