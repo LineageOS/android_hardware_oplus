7ac6ddb7a0e450fe3a8c4e82774b3af93bf67d83
Bruno Martins <bgcngm@gmail.com>
2025-10-26 14:35:06 +0000

    [WIP] Introduce eSIM switcher app
    Change-Id: I28216910c39f4ee690d2e69d39158413422bfe8d

---
+++ eSimSwitcher/src/org/lineageos/settings/esimswitcher/EsimSettingsFragment.kt
@@ -0,0 +1,61 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.esimswitcher
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.ServiceManager
+import android.util.Log
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
+import org.lineageos.settings.esimswitcher.R
+import vendor.oplus.hardware.esim.IOplusEsim
+
+class EsimSettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
+    private val handler = Handler(Looper.getMainLooper())
+
+    private val oplusEsimService by lazy {
+        IOplusEsim.Stub.asInterface(ServiceManager.getService("${IOplusEsim.DESCRIPTOR}/default"))
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.esim_settings, rootKey)
+
+        val switcher = findPreference<SwitchPreferenceCompat>(ESIM_TOGGLE_KEY)
+        switcher?.isChecked = oplusEsimService?.esimGpio == 1
+        switcher?.onPreferenceChangeListener = this
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        handler.post {
+            changeEsimState(if (newValue as Boolean) 1 else 0)
+        }
+
+        return true
+    }
+
+    private fun changeEsimState(state: Int) {
+        val gpio = oplusEsimService?.esimGpio ?: return
+        Log.d(TAG, "Current eSIM status = $gpio")
+
+        if (state == gpio) {
+            Log.d(TAG, "No need to change eSIM state")
+            return
+        }
+
+        oplusEsimService.setUimPower(0)
+        oplusEsimService.setEsimGpio(if (gpio == 0) 1 else 0)
+        oplusEsimService.setUimPower(1)
+    }
+
+    companion object {
+        private const val TAG = "OplusEsimSwitcher"
+
+        private const val ESIM_TOGGLE_KEY = "esim_toggle"
+    }
+}
