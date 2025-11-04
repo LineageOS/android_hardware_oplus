d204c7fac1ccf9c815556c89fd22834bf84ce1e5
anky894 <ko282ko82@gmail.com>
2025-11-04 23:31:46 +0530

    oplus: Introduce dummy liboplus-uah-client
    Change-Id: Ifc628f858a38ea9976a5796fdf8a9193e3281524

---
+++ uah-client/uah-client.c
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int uahInit(...) { return 0; }
+
+int UahNotifyWapper(...) { return 0; }
+int UahNotify(...) { return 0; }
+
+int UahEventAcquire(...) { return 0; }
+int UahEventAcquireWrapper(...) { return 0; }
+
+int UahResAcquire(...) { return 0; }
+int UahPlatformResAcquire(...) { return 0; }
+
+int UahRelease(...) { return 0; }
+int UahReleaseWapper(...) { return 0; }
+
+int UahResStateRequest(...) { return 0; }
+int UahMutiResStateRequest(...) { return 0; }
+
+int UahGetHistory(...) { return 0; }
+int UahGetPowerConsis(...) { return 0; }
+int UahGetPMStatus(...) { return 0; }
+
+int Uah_fetch_dataset(...) { return 0; }
+
+int uahRuleCtl(...) { return 0; }
