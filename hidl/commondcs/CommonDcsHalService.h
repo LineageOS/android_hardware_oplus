3b23a36e1a312291e535714f9fb9b1c79537467c
Roman Rihter <teledurak@gmail.com>
2025-11-07 05:01:02 +0700

    hidl: Add vendor.oplus.hardware.commondcs@1.0-service
    Change-Id: I2989536766da21cefd17252a96ced4be7ab996ab

---
+++ hidl/commondcs/CommonDcsHalService.h
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <vendor/oplus/hardware/commondcs/1.0/ICommonDcsHalService.h>
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+
+namespace vendor::oplus::hardware::commondcs::V1_0::implementation {
+
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+
+struct CommonDcsHalService : public ICommonDcsHalService {
+    Return<int32_t> notifyMsgToCommonDcs(const hidl_vec<::vendor::oplus::hardware::commondcs::V1_0::StringPair>& data, const hidl_string& logTag, const hidl_string& eventId) override;
+};
+
+} // namespace vendor::oplus::hardware::commondcs::V1_0::implementation
