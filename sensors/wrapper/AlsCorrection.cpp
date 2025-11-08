aae345ed5ba6c7cfe667fdf50f7e66f8a4b904bd
dianlujitao <dianlujitao@lineageos.org>
2025-11-08 15:57:13 +0000

    sensors: wrapper: Integrate with ALS correction
    Change-Id: Ie89f9c53045ca254dd8a1b5a5cc7cc8e977a4754

--- sensors/wrapper/AlsCorrection.cpp
+++ sensors/wrapper/AlsCorrection.cpp
@@ -376,7 +376,7 @@ bool AlsCorrection::init() {
     return true;
 }
 
-void AlsCorrection::process(Event& event) {
+float AlsCorrection::process(const Event& event) {
     LOG(VERBOSE) << "Raw sensor reading: " << event.u.scalar;
 
     std::string buf;
@@ -399,8 +399,7 @@ void AlsCorrection::process(Event& event) {
         }
         if ((now - last_update_) < ms2ns(100)) {
             LOG(WARNING) << "Events coming too fast, dropping";
-            event.sensorHandle = 0;
-            return;
+            return -1.f;
         }
         last_update_ = now;
     }
@@ -622,8 +621,8 @@ void AlsCorrection::process(Event& event) {
         final_lux = calculated_lux;
     }
 
-    event.u.scalar = final_lux;
-    LOG(VERBOSE) << "Final corrected lux: " << event.u.scalar;
+    LOG(VERBOSE) << "Final corrected lux: " << final_lux;
+    return final_lux;
 }
 
 }  // namespace wrapper
