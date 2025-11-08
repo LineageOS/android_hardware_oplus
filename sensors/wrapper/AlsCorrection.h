3cfa7726a3f426a34cb44d673d866c8102d63164
dianlujitao <dianlujitao@lineageos.org>
2025-11-08 15:57:13 +0000

    sensors: wrapper: Integrate with ALS correction
    Change-Id: Ie89f9c53045ca254dd8a1b5a5cc7cc8e977a4754

--- sensors/wrapper/AlsCorrection.h
+++ sensors/wrapper/AlsCorrection.h
@@ -125,7 +125,7 @@ using aidl::vendor::lineage::oplus_als::IAreaCapture;
 class AlsCorrection {
   public:
     bool init();
-    void process(Event& event);
+    float process(const Event& event);
 
   private:
     bool loadFusionLightConfig();
