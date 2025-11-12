0a13697972c98257ed66a3431858260c907fcefb
dianlujitao <dianlujitao@lineageos.org>
2025-11-12 00:39:28 +0000

    als: Let caller specify screenshot area
    * Refactor code to have the caller provide the screenshot area,
      improving flexibility and clarity.
    * Also includes general cleanup for better maintainability.
    
    Change-Id: Ib19120eea626c979e3a5f79787df2f18f0162ef6

--- aidl/als/AreaCapture.h
+++ aidl/als/AreaCapture.h
@@ -1,12 +1,11 @@
 /*
- * SPDX-FileCopyrightText: 2021-2024 The LineageOS Project
+ * SPDX-FileCopyrightText: 2021-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
 
 #include <aidl/vendor/lineage/oplus_als/BnAreaCapture.h>
-#include <ui/Rect.h>
 
 namespace aidl {
 namespace vendor {
@@ -15,13 +14,9 @@ namespace oplus_als {
 
 class AreaCapture : public BnAreaCapture {
   public:
-    AreaCapture();
-    ndk::ScopedAStatus getAreaBrightness(AreaRgbCaptureResult* _aidl_return) override;
-
-  private:
-    static ::android::sp<::android::IBinder> getInternalDisplayToken();
-
-    ::android::Rect m_screenshot_rect;
+    ndk::ScopedAStatus getAreaBrightness(int32_t topX, int32_t topY, int32_t bottomX,
+                                         int32_t bottomY,
+                                         AreaRgbCaptureResult* _aidl_return) override;
 };
 
 }  // namespace oplus_als
