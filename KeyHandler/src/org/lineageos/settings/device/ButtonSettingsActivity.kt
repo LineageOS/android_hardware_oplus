429ad0a0d35967e5b2ecaa7645e6f1d5a01587ad
Bruno Martins <bgcngm@gmail.com>
2025-11-23 21:51:06 +0000

    KeyHandler: Convert ButtonSettings for M3E support
    Change-Id: I8bde758a033851ca040660f005bf90589810f0da

--- KeyHandler/src/org/lineageos/settings/device/ButtonSettingsActivity.kt
+++ KeyHandler/src/org/lineageos/settings/device/ButtonSettingsActivity.kt
@@ -1,18 +1,17 @@
 /*
- * Copyright (C) 2021-2024 The LineageOS Project
+ * SPDX-FileCopyrightText: 2021-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.lineageos.settings.device
 
 import android.os.Bundle
-
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
 
 class ButtonSettingsActivity : CollapsingToolbarBaseActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fragmentManager.beginTransaction().replace(
+        supportFragmentManager.beginTransaction().replace(
             com.android.settingslib.collapsingtoolbar.R.id.content_frame,
             ButtonSettingsFragment()
         ).commit()
--- KeyHandler/src/org/lineageos/settings/device/ButtonSettingsFragment.kt
+++ KeyHandler/src/org/lineageos/settings/device/ButtonSettingsFragment.kt
@@ -1,31 +1,15 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * SPDX-FileCopyrightText: 2021-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.lineageos.settings.device
 
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.preference.PreferenceFragment
+import com.android.settingslib.widget.SettingsBasePreferenceFragment
 
-class ButtonSettingsFragment : PreferenceFragment() {
+class ButtonSettingsFragment : SettingsBasePreferenceFragment() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        addPreferencesFromResource(R.xml.button_panel)
-        activity.actionBar!!.setDisplayHomeAsUpEnabled(true)
-    }
-
-    override fun addPreferencesFromResource(preferencesResId: Int) {
-        super.addPreferencesFromResource(preferencesResId)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.home -> {
-                activity.finish()
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
+        setPreferencesFromResource(R.xml.button_panel, rootKey)
     }
 }
