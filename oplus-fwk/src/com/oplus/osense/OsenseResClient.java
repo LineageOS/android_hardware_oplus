caab579348eaa4082d9166c4b5db0e90ad33b270
chandu078 <chandudyavanapelli03@gmail.com>
2025-11-27 20:29:28 +0000

    oplus-fwk: Add OsenseResClient and OplusUIFirstManager stub implementation
    Required for OOS 16.0.0 blobs.
    
    Change-Id: Ie3921eb1f62771885f69ad76a570def89e9286ac

---
+++ oplus-fwk/src/com/oplus/osense/OsenseResClient.java
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.oplus.osense;
+
+public class OsenseResClient {
+    public static OsenseResClient get(Class clazz) {
+        return null;
+    }
+}
