73d46a9e6387a0d9412806ac70586cef27d4bdd1
jabashque <jabashque@gmail.com>
2026-01-24 06:11:00 -0800

    vibrator: add setAmplitude support for LedVibratorDevice
    Amplitude is scaled non-linearly so that low amplitude vibrations
    actually feel like they're low amplitude.
    
    Change-Id: Iec96995614380341f2bee4e4ba55cf1e05455691

--- aidl/vibrator/qti/Vibrator.cpp
+++ aidl/vibrator/qti/Vibrator.cpp
@@ -37,6 +37,7 @@
 #include <log/log.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <cmath>
 #include <thread>
 
 #include "include/Vibrator.h"
@@ -379,16 +380,13 @@ int LedVibratorDevice::on(int32_t timeoutMs) {
     int ret = 0;
     if (timeoutMs <= 0) {
         return ret;
-    } else if (timeoutMs <= 20) {
-        ret |= write_value(LED_DEVICE "/vmax", timeoutMs * 10);
     } else {
-        ret |= write_value(LED_DEVICE "/vmax", 1600);
+        ret |= write_value(LED_DEVICE "/vmax", mSavedVmax);
     }
     ret |= write_value(LED_DEVICE "/waveform_index", 7);
     ret |= write_value(LED_DEVICE "/duration", timeoutMs);
     ret |= write_value(LED_DEVICE "/state", "1");
     ret |= write_value(LED_DEVICE "/activate", "1");
-    ret |= write_value(LED_DEVICE "/activate", "0");
 
     return ret;
 }
@@ -411,12 +409,25 @@ int LedVibratorDevice::off() {
     ret = write_value(file, "0");
     return ret;
 }
+int LedVibratorDevice::setAmplitude(float amplitude) {
+    int ret = 0;
+
+    // vmax values <= 1 emit practically nothing, so ensure the zero
+    // value will correspond to vmax of 1.
+    mSavedVmax = 1 + (int)std::ceil(std::pow(amplitude, 1.5) * 254);
+    // write to vmax immediately because setAmplitude is supposed
+    // to be able to change the currently playing vibration's amplitude
+    // too.
+    ret |= write_value(LED_DEVICE "/vmax", mSavedVmax);
+    return ret;
+}
 
 ndk::ScopedAStatus Vibrator::getCapabilities(int32_t* _aidl_return) {
     *_aidl_return = IVibrator::CAP_ON_CALLBACK;
 
     if (ledVib.mDetected) {
         *_aidl_return |= IVibrator::CAP_PERFORM_CALLBACK;
+        *_aidl_return |= IVibrator::CAP_AMPLITUDE_CONTROL;
         ALOGD("QTI Vibrator reporting capabilities: %d", *_aidl_return);
         return ndk::ScopedAStatus::ok();
     }
@@ -565,19 +576,21 @@ ndk::ScopedAStatus Vibrator::setAmplitude(float amplitude) {
     uint8_t tmp;
     int ret;
 
-    if (ledVib.mDetected)
-        return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
-
     ALOGD("Vibrator set amplitude: %f", amplitude);
 
     if (amplitude <= 0.0f || amplitude > 1.0f)
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_ILLEGAL_ARGUMENT));
 
-    if (ff.mInExternalControl)
-        return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+    if (ledVib.mDetected) {
+        ret = ledVib.setAmplitude(amplitude);
+    } else {
+        if (ff.mInExternalControl)
+            return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
 
-    tmp = (uint8_t)(amplitude * 0xff);
-    ret = ff.setAmplitude(tmp);
+        tmp = (uint8_t)(amplitude * 0xff);
+        ret = ff.setAmplitude(tmp);
+    }
+
     if (ret != 0) return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_SERVICE_SPECIFIC));
 
     return ndk::ScopedAStatus::ok();
