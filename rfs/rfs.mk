492ed5f462ea009e70154a3df119a2d0ddc7f6d8
chandu078 <chandudyavanapelli03@gmail.com>
2026-02-21 12:33:01 +0000

    oplus: Add dycnvbk MPSS symlinks
    As seen on OnePlus SM8850 devices.
    
    Change-Id: Ie2b550762c7340817ef193667a5bd07a69ec612a

---
+++ rfs/rfs.mk
@@ -0,0 +1,7 @@
+# RFS MDM MPSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_mdm_mpss_readonly_dycnvbk_symlink
+
+# RFS MSM MPSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_mpss_readonly_dycnvbk_symlink
