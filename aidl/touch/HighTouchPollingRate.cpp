069917c6707f8406472e2df7a1df2ad960e4e72e
kmiit <kmiit@foxmail.com>
2026-03-28 09:33:54 +0800

    touch: Fix HighTouchPollingRate of OplusTouch node value
    Tested on pista:
    10 for 280hz, 11 for 340hz
    
    Change-Id: I76d7a9797788e1b92589f5e1e11746594600023a

--- aidl/touch/HighTouchPollingRate.cpp
+++ aidl/touch/HighTouchPollingRate.cpp
@@ -48,7 +48,7 @@ ndk::ScopedAStatus HighTouchPollingRate::setEnabled(bool enable) {
     if (mOplusTouch) {
         mOplusTouch->touchWriteNodeFileOneWay(OplusTouchConstants::DEFAULT_TP_IC_ID,
                                               OplusTouchConstants::GAME_SWITCH_ENABLE_NODE,
-                                              enable ? "1" : "0");
+                                              enable ? "10" : "0");
     } else if (!WriteStringToFile(enable ? "1" : "0", kGameSwitchEnablePath, true)) {
         LOG(ERROR) << "Failed to write HighTouchPollingRate state";
         return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
