e5d62db1e7af39f0b24460d81266479bbb1e141e
LuK1337 <priv.luk@gmail.com>
2026-06-24 10:54:27 +0200

    test
    Test: adb shell getevent -r
    Change-Id: Id9ae1bbc3dff33e77b4a27d977efa7a7c9b50a74

--- aidl/touch/HighTouchPollingRate.cpp
+++ aidl/touch/HighTouchPollingRate.cpp
@@ -27,7 +27,7 @@ namespace lineage {
 namespace touch {
 
 HighTouchPollingRate::HighTouchPollingRate(std::shared_ptr<IOplusTouch> oplusTouch)
-    : mOplusTouch(std::move(oplusTouch)) {}
+    : mOplusTouch(std::move(oplusTouch)), mHighestSupportedMode(highestSupportedMode()) {}
 
 ndk::ScopedAStatus HighTouchPollingRate::getEnabled(bool* _aidl_return) {
     std::string value;
@@ -56,6 +56,45 @@ ndk::ScopedAStatus HighTouchPollingRate::setEnabled(bool enable) {
     }
 
     return ndk::ScopedAStatus::ok();
+}
+
+std::string HighTouchPollingRate::highestSupportedMode() {
+    if (mOplusTouch) {
+        int aidl_return = 0;
+
+        // Backup current mode
+        std::string backup;
+
+        mOplusTouch->touchReadNodeFile(OplusTouchConstants::DEFAULT_TP_IC_ID,
+                                       OplusTouchConstants::GAME_SWITCH_ENABLE_NODE, &backup);
+
+        // Find highest supported game switch mode
+        std::string ret(backup);
+
+        for (int i = 20; i > 0; i--) {
+            std::string value = std::to_string(i);
+
+            mOplusTouch->touchWriteNodeFile(OplusTouchConstants::DEFAULT_TP_IC_ID,
+                                            OplusTouchConstants::GAME_SWITCH_ENABLE_NODE, value,
+                                            &aidl_return);
+            mOplusTouch->touchReadNodeFile(OplusTouchConstants::DEFAULT_TP_IC_ID,
+                                           OplusTouchConstants::GAME_SWITCH_ENABLE_NODE, &ret);
+
+            if (value == ret) {
+                LOG(INFO) << "HighTouchPollingRate highest supported mode = " << value;
+                break;
+            }
+        }
+
+        // Restore previous mode
+        mOplusTouch->touchWriteNodeFile(OplusTouchConstants::DEFAULT_TP_IC_ID,
+                                        OplusTouchConstants::GAME_SWITCH_ENABLE_NODE, backup,
+                                        &aidl_return);
+
+        return ret;
+    } else {
+        return "1";
+    }
 }
 
 }  // namespace touch
